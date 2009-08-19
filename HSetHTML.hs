2a61f75191143e0d7c55ec451512561a9b45f113
rsw <rsw@e009f53e-6bd9-0310-94dc-a54994e6c07e>
2009-08-19 06:56:28 +0000

    time-based scoring with elapsed time display
    git-svn-id: https://svn.jfet.org/rsw/src/haskell/hset@448 e009f53e-6bd9-0310-94dc-a54994e6c07e

--- HSetHTML.hs
+++ HSetHTML.hs
@@ -26,11 +26,18 @@ import System.Random (mkStdGen,randomIO)
 import qualified Data.Digest.MD5 as MD5
 import qualified Codec.Binary.BubbleBabble as BuBa
 import Data.Char (ord)
-import System.Time (getClockTime,TOD(..))
+import System.Time (getClockTime,ClockTime(..))
 
 #ifndef __SERVER_SECRET_KEY__
 #define __SERVER_SECRET_KEY__ "asdf"
 #endif
+
+-- directory where images are stored
+imgDir = "/~kwantam/images/cards/"
+
+-- extract seconds from a ClockTime
+getClockSeconds :: (Integral a) => ClockTime -> a
+getClockSeconds (TOD a _) = fromIntegral a
 
 -- this gets set either in the ifndef above or (preferably)
 -- during build with the -D commandline switch
@@ -44,21 +51,26 @@ clickScript o cx = "document.sform."++cx++".checked = ! document.sform."++cx++".
 
 -- kind of the central thingie
 -- handle inputs from the user, dispatch display, update the deck, et cetera
-newPage [] _ s _ = endGame "You win!" s
-newPage d  n s r | take 1 (identifySets $ decompressDeck d) == [] = endGame "No more sets." s
-                 | take 1 (identifySets $ decompressDeck (take n d)) == [] = newPage d (n+3) s [-1]
-                 | otherwise = if r == [-1] then writePage d n "Here, have some more cards." s else
-                               if isSetResponse $ decompressDeck r 
-                                then if n==12 && length d > 12
-                                      then writePage (replaceCards r ((drop n).(take (n+3)) $ d)
-                                                                     ((take n d)++(drop (n+3) d)))
-                                                     12 "Correct!" (s+1)
-                                      else writePage (d \\ r) (min (n-3) (length d)) "Correct!" (s+1)
-                                else writePage d n "Sorry, try again." (s-1)
+newPage [] _ s t ct _ = endGame "You win!" s t ct
+newPage d  n s t ct r
+        | take 1 (identifySets $ decompressDeck d) == [] = endGame "No more sets." s t ct
+        | take 1 (identifySets $ decompressDeck (take n d)) == [] = newPage d (n+3) s t ct [-1]
+        | otherwise = if r == [-1] then writePage d n "Here, have some more cards." s t ct else
+                       if isSetResponse $ decompressDeck r 
+                        then if n==12 && length d > 12
+                              then writePage (replaceCards r ((drop n).(take (n+3)) $ d)
+                                                             ((take n d)++(drop (n+3) d)))
+                                             12 "Correct!" (s+1) t ct
+                              else writePage (d \\ r) (min (n-3) (length d)) "Correct!" (s+1) t ct
+                        else writePage d n "Sorry, try again." (s-1) t ct
 
 -- we display this when the game is over
-endGame m s = h1 << m +++ h3 << ("Final score: " ++ show s)
-                      +++ form << [paragraph << submit "" "Play again?"]
+endGame m s t ct = h1 << m +++ h3 << ("Final score: " ++ show s)
+                           +++ h3 << ("Time elapsed: " ++ show (t - ct))
+			   +++ h3 << ("Composite: " ++ show score)
+                           +++ form << [paragraph << submit "" "Play again?"]
+    where score | t - ct > 600 = s
+		| otherwise = s * ceiling ((600 / fromIntegral (t - ct))^2)
 
 -- is the response from the user a set?
 isSetResponse (r1:r2:r3:[]) = isSet r1 r2 r3
@@ -70,23 +82,28 @@ everyThree _ ds         = ds
 
 -- given the deck, number of cards, and a message,
 -- display the page
-writePage d n m s = h3 << m +++ 
-                    paragraph << instr +++ 
-                    form ! [method "POST", name "sform"]
-                         << ( hidden "setDeck" dc : 
-                              hidden "numCards" nc :
-                              hidden "verToken" ( vtok d n s ) :
-                              hidden "userScore" sc :
-                              cardsOnTable ++
-                              [br +++ submit "" "Submit" +++ spaceHtml +++ ("Score: " ++ sc)] )
+writePage d n m s t ct = h3 << m +++ 
+                      paragraph << instr +++ 
+                      form ! [method "POST", name "sform"] <<
+                           ( hidden "setDeck" dc : 
+                             hidden "numCards" nc :
+                             hidden "verToken" ( vtok d n s ct ) :
+                             hidden "userScore" sc :
+			     hidden "clockTime" tc :
+                             cardsOnTable ++
+                             [br +++ submit "" "Submit" +++ 
+			      spaceHtml +++ ("Score: " ++ sc) +++ 
+			      spaceHtml +++ ("Time elapsed: " ++ etc) ] )
     where dc = show d
           nc = show n
           sc = show s
+	  tc = show ct
+	  etc = show (t - ct)
           cardsOnTable = everyThree br $ map cardHTML $ zip [0..] (take n d)
           cardHTML (x,c) = (image ! [src oImg,
 			             onClick $ clickScript oImg cx])
                             +++ spaceHtml +++ checkbox cx "c"
-              where oImg = "/~kwantam/images/cards/" ++ show c ++ ".png"
+              where oImg = imgDir ++ show c ++ ".png"
                     cx = 'c':show x
 	  instr = "Choose a set, or just hit Submit if you think there aren't any."
 
@@ -100,26 +117,28 @@ takeChecked (d:dc) (r:rc) = if r == "c"
 
 -- verification token: BubbleBabble encoded md5 hash of the ascii encoding
 -- of the server secret key, the deck, the number of cards dealt, and the score
-vtok d n s = BuBa.encode $ MD5.hash $ map (fromIntegral.ord) $ show (serverSecretKey,d,n,s)
+vtok d n s ct = BuBa.encode $ MD5.hash $ map (fromIntegral.ord) $ show (serverSecretKey,d,n,s,ct)
 
 -- generate a new shuffled deck and display the welcome page
-showGreeting n = writePage dd 12 "Welcome!" 0
+showGreeting n t = writePage dd 12 "Welcome!" 0 t t
     where dd = compressDeck $ evalState mkRandDeck n
 
 -- dispatch
 -- get the form inputs
 -- dispatch a response if we get a good verification token,
 -- otherwise start a new game via showGreeting
-cgiMain n = do nc <- liftM (read.(fromMaybe "0")) $ getInput "numCards"
-               dc <- liftM (read.(fromMaybe "[]")) $ getInput "setDeck"
-               rz <- mapM ((liftM $ fromMaybe "[]").getInput) $ take nc (map (('c':).show) [0..])
-               vt <- liftM (fromMaybe "") $ getInput "verToken"
-               sc <- liftM (read.(fromMaybe "0")) $ getInput "userScore"
-               if vt /= vtok dc nc sc
-                then output $ renderHtml $ body << (showGreeting n)
-                else output $ renderHtml $ body << (newPage dc nc sc $ takeChecked dc rz)
+cgiMain n t = do nc <- liftM (read.(fromMaybe "0")) $ getInput "numCards"
+                 dc <- liftM (read.(fromMaybe "[]")) $ getInput "setDeck"
+                 rz <- mapM ((liftM $ fromMaybe "[]").getInput) $ take nc (map (('c':).show) [0..])
+                 vt <- liftM (fromMaybe "") $ getInput "verToken"
+                 sc <- liftM (read.(fromMaybe "0")) $ getInput "userScore"
+		 ct <- liftM (read.(fromMaybe "0")) $ getInput "clockTime"
+                 if vt /= vtok dc nc sc ct
+                  then output $ renderHtml $ body << (showGreeting n t)
+                  else output $ renderHtml $ body << (newPage dc nc sc t ct $ takeChecked dc rz)
 
 -- not much here, keep the IO monad stuff in the main function
 -- and dispatch the CGI handler
 main = do sg <- liftM mkStdGen randomIO
-          runCGI $ handleErrors (cgiMain sg)
+	  ct <- liftM getClockSeconds getClockTime
+          runCGI $ handleErrors (cgiMain sg ct)
